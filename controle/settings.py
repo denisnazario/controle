7101830d770caa09ad8ae92f74cc43d9d80073e4
denis.nazario <denis.nazario@unigranrio.br>
2020-11-09 22:20:46 -0300

    key oculted

--- controle/settings.py
+++ controle/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wn8iwm@5uori-x&84*#8b*d76u!h_+@kuq0f+-8&&98)0bn*%0'
+SECRET_KEY = DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
